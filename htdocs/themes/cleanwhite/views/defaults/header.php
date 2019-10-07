f01b8b21e834b95b95462dfab0326c0a4443e4f3
Firdaus Ahmad <hello@daus.xyz>
2019-10-08 04:02:35 +0800

    Fixed theme

--- htdocs/themes/cleanwhite/views/defaults/header.php
+++ htdocs/themes/cleanwhite/views/defaults/header.php
@@ -51,14 +51,16 @@ $this->carabiner->display('css');
 					<ul class="tabs">
 						<?php $l = $this->uri->segment(1)?>
 						<li><a <?php if($l == ""){ echo 'class="active"'; }?> href="<?php echo base_url()?>" title="<?php echo lang('menu_create_title'); ?>"><?php echo lang('menu_create'); ?></a></li>
-<?php if(! $this->config->item('private_only')){ ?>
+<?php if ($this->config->item('private_only') || $this->config->item('disable_recent')) {}else{ ?>
 						<li><a <?php if($l == "lists"){ echo 'class="active"'; }?> href="<?php echo site_url('lists'); ?>" title="<?php echo lang('menu_recent_title'); ?>"><?php echo lang('menu_recent'); ?></a></li>
 						<li><a <?php if($l == "trends"){ echo 'class="active"'; }?> href="<?php echo site_url('trends'); ?>" title="<?php echo lang('menu_trending_title'); ?>"><?php echo lang('menu_trending'); ?></a></li>
 <?php } ?>
 <?php if(! $this->config->item('disable_api')){ ?>
 						<li><a  <?php if($l == "api"){ echo 'class="active"'; }?> href="<?php echo site_url('api'); ?>" title="<?php echo lang('menu_api'); ?>"><?php echo lang('menu_api'); ?></a></li>
 <?php } ?>
+<?php if ($this->config->item('private_only') || $this->config->item('disable_about')) {}else{ ?>
 						<li><a  <?php if($l == "about"){ echo 'class="active"'; }?> href="<?php echo site_url('about'); ?>" title="<?php echo lang('menu_about'); ?>"><?php echo lang('menu_about'); ?></a></li>
+<?php } ?>
                         <?php
                             if ($this->config->item('require_auth') ){
                                 if ($this->auth_ldap->is_authenticated()){
